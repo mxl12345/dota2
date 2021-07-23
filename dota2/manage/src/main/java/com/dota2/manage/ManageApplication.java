32b2d28c590d3d1ecdaece9ff418bb3aae40c457
mxl12345 <54735983+mxl12345@users.noreply.github.com>
2021-07-24 00:22:58 +0800

    Add files via upload

---
+++ dota2/manage/src/main/java/com/dota2/manage/ManageApplication.java
@@ -0,0 +1,13 @@
+package com.dota2.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class, args);
+    }
+
+}
